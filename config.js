864d8ff9bee11671711a2c4f8bec6de16753a4fd
nminhfr <85264436+nminhfr@users.noreply.github.com>
2021-06-04 07:47:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Ánh à!',
+    introTitle: 'Phương à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Ánh cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Phương cứ đi nhầm đường
+    Lạc vào tim Minh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Tôi chỉ muốn nói với Ánh rằng tôi rất yêu ánh 😙',
-    desc: 'Tôi thích Ánh vì nụ cười Ánh,mắt của Ánh,nói chung là tôi thích Ánh ',
+    btnIntro: 'Mời Phương vào',
+    title: 'Tôi chỉ muốn nói với Phương rằng tôi rất yêu Phương 😙',
+    desc: 'Tôi thích Phương vì nụ cười Phương,mắt của Phương,nói chung là tôi thích Phương, Phương làm ny tui nha ',
     btnYes: 'Tui cũng yêu cậu <33',
     btnNo: 'Không, chúng ta chỉ là bạn :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Ánh lại yêu tôi <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Phương lại yêu tôi <3',
+    btnReply: 'Gửi cho Minh <3',
     reply: 'Yêu MInh nhiều lắm <33333333',
-    mess: 'Tôi biết mà 🥰. Yêu Ánh nhiều nhiều 😘😘',
-    messDesc: 'Tối 7h tôi qua đón Ánh đi chơi nhó.Yêu em <333.',
+    mess: 'Tôi biết mà 🥰. Yêu Phương nhiều nhiều 😘😘',
+    messDesc: 'Tối 7h tôi qua đón Phương đi chơi nhó.Yêu Phương <333.',
     btnAccept: 'Yêu anh <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
