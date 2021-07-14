8f1c6b8b0b91c48eb9700d0ddbd8402a21e1e645
nminhfr <85264436+nminhfr@users.noreply.github.com>
2021-07-14 11:35:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Mai Chi à!',
+    introTitle: 'Tiểu Yến à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Chi cứ đi nhầm đường
-    Lạc vào tim Linh lẻ loi
+    Mà sao bae cứ đi nhầm đường
+    Lạc vào tim Minh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'Mời Chi vào thế giới của tui',
-    title: 'Tôi chỉ muốn nói với Chi rằng tôi rất yêu Chi 😙',
-    desc: 'Tôi thích Chi vì nụ cười Chi,mắt của Chi,nói chung là tôi thích Chi, Chi làm ny tui nha ',
+    btnIntro: 'Mời Yến vào thế giới của tui',
+    title: 'Tôi chỉ muốn nói với Yến rằng tôi rất yêu em 😙',
+    desc: 'Tôi thích em vì nụ cười,mắt của em,nói chung là tôi thích em, Yến làm ny tui nha ',
     btnYes: 'Tui cũng yêu cậu <33',
     btnNo: 'Không, chúng ta chỉ là bạn thôi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Chi lại yêu tôi <3',
-    btnReply: 'Gửi cho Linh <3',
-    reply: 'Yêu Linh nhiều nhiều <33333333',
-    mess: 'Tôi biết mà 🥰. Yêu Chi nhiều nhiều 😘😘',
-    messDesc: 'Tối 7h tôi qua đón Chi đi chơi nhó.Yêu Chi <333.',
-    btnAccept: 'Yêu Linh <3',
-    messLink: 'http://fb.com' //https://www.facebook.com/NhatLinh8Tuoi
-}
+    question:'Trên thế giới hơn 7 tỉ người mà sao Yến lại chọn yêu tôi <3',
+    btnReply: 'Gửi cho Minh <3',
+    reply: 'Thích thì yêu mà không thích thì yêu',
+    mess: 'Tôi biết mà 🥰. Yêu Yến nhiều nhiều 😘😘',
+    messDesc: 'Đợi hết dịch rồi xuống đây chơi với t nhé. Yêu YẾN <333.',
+    btnAccept: 'Yêu Minh.<3',
+    messLink: 'http://https://www.facebook.com/nhatminh.nguyentran.583}
+
